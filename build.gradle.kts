02fccad47c42c6405f032e1b8fb984f3d9d634c6
Daniil Demidko <demidko@farpost.com>
2021-12-13 00:11:03 +1000

    обновления

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,8 +9,8 @@ plugins {
   kotlin("plugin.serialization") version "1.6.0"
 }
 dependencies {
-  api("org.jetbrains.kotlinx:kotlinx-serialization-cbor:1.3.1")
   api("io.github.kotlin-telegram-bot.kotlin-telegram-bot:telegram:6.0.6")
+  implementation("org.jetbrains.kotlinx:kotlinx-serialization-cbor:1.3.1")
   testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
   testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.25")
   testImplementation("io.mockk:mockk:1.12.1")
