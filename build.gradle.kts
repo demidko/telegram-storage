661c4d41f1a0af049070665a47eadab2df8c414f
Daniel Demidko <demidko@farpost.com>
2022-07-29 16:23:15 +0500

    deps updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,15 +5,15 @@ repositories {
 plugins {
   `java-library`
   `maven-publish`
-  kotlin("jvm") version "1.7.0-RC"
-  kotlin("plugin.serialization") version "1.7.0-RC"
+  kotlin("jvm") version "1.7.20-Beta"
+  kotlin("plugin.serialization") version "1.7.20-Beta"
 }
 dependencies {
   api("io.github.kotlin-telegram-bot.kotlin-telegram-bot:telegram:6.0.7")
-  api("org.jetbrains.kotlinx:kotlinx-serialization-cbor:1.3.3")
-  testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+  api("org.jetbrains.kotlinx:kotlinx-serialization-cbor:1.4.0-RC")
+  testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
   testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.25")
-  testImplementation("io.mockk:mockk:1.12.4")
+  testImplementation("io.mockk:mockk:1.12.5")
 }
 tasks.compileKotlin {
   kotlinOptions.freeCompilerArgs += "-Xopt-in=kotlinx.serialization.ExperimentalSerializationApi"
